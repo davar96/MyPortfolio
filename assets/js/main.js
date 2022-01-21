69b3849df96e1e3f7cb7ef647023baa0e33e81fd
David Arustamyan <david.arustamyan.96@gmail.com>
2022-01-21 23:06:34 +0100

    worked on the qualification part (need to come back to fix bugs )

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,26 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+// Qualification Tabs
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+
+        tab.classList.add('qualification__active')
+    })
+})
